12a7d4382300d1e527e378539151c7949ab396b6
Abhinav <abhinav.sachdeva9@gmail.com>
2025-09-02 17:50:51 -0400

    some more changes for test

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,7 @@ app.use('/api', apiRouter)
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+const PORT = parseInt(process.env.PORT || "3000", 10);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
